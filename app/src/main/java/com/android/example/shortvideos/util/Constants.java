ef0a057d900d49ffc976cf90000f3f3da8db016d
Akash <91256425+Akash91746@users.noreply.github.com>
2021-10-04 18:08:32 +0530

    Delete Constants.java

--- app/src/main/java/com/android/example/shortvideos/util/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.android.example.shortvideos.util;
-
-public class Constants {
-    public static final String BASE_URL = "http://fatema.takatakind.com/";
-
-    public static final int EXO_PLAYER_CACHE_SIZE = 100 * 1024 * 1024;
-}
